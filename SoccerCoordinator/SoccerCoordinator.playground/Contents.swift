9edfca81aa25d0669c0f9452ffa33b8e0cb433fc
Katherine Ebel <kathyebel@me.com>
2016-10-12 08:15:57 -0400

    Initial Commit

---
+++ SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
@@ -0,0 +1,43 @@
+//: Playground - noun: a place where people can play
+
+let joeSmith: [String : Any] = ["name": "Joe Smith", "height": 42, "isExperienced": true, "guardians": ["Jim Smith", "Jan Smith"]]
+
+let jillTanner: [String : Any] = ["name": "Jill Tanner", "height": 36, "isExperienced": true, "guardians": ["Clara Tanner"]]
+
+let billBon: [String : Any] = ["name": "Bill Bon", "height": 43, "isExperienced": true, "guardians": ["Sara Bon", "Jenny Bon"]]
+
+let evaGordon: [String : Any] = ["name": "Eva Gordon", "height": 45, "isExperienced": false, "guardians": ["Wendy Gordon", "Mike Gordon"]]
+
+let mattGill: [String : Any] = ["name": "Matt Gill", "height": 40, "isExperienced": false, "guardians": ["Charles Gill", "Sylvia Gill"]]
+
+let kimmyStein: [String : Any] = ["name": "Kimmy Stein", "height": 41, "isExperienced": false, "guardians": ["Bill Stein", "Hillary Stein"]]
+
+let sammyAdams: [String : Any] = ["name": "Sammy Adams", "height": 45, "isExperienced": false, "guardians": ["Jeff Adams"]]
+
+let karlSaygan: [String : Any] = ["name": "Karl Saygan", "height": 42, "isExperienced": true, "guardians": ["Heather Bledsoe"]]
+
+let suzaneGreenberg: [String : Any] = ["name": "Suzane Greenberg", "height": 44, "isExperienced": true, "guardians": ["Henrietta Dumas"]]
+
+let salDali: [String : Any] = ["name": "Sal Dali", "height": 41, "isExperienced": false, "guardians": ["Gala Dali"]]
+
+let joeKavalier: [String : Any] = ["name": "Joe Kavalier", "height": 39, "isExperienced": false, "guardians": ["Sam Kavalier", "Elaine Kavalier"]]
+
+let benFinkelstein: [String : Any] = ["name": "Ben Finkelstein", "height": 44, "isExperienced": false, "guardians": ["Aaron Finkelstein", "Jill Finkelstein"]]
+
+let diegoSoto: [String : Any] = ["name": "Diego Soto", "height": 41, "isExperienced": true, "guardians": ["Robin Soto", "Sarika Soto"]]
+
+let chloeAlaska: [String : Any] = ["name": "Chloe Alaska", "height": 47, "isExperienced": false, "guardians": ["David Alaska", "Jamie Alaska"]]
+
+let arnoldWillis: [String : Any] = ["name": "Arnold Willis", "height": 43, "isExperienced": false, "guardians": ["Claire Willis"]]
+
+let phillipHelm: [String : Any] = ["name": "Phillip Helm", "height": 44, "isExperienced": true, "guardians": ["Thomas Helm", "Eva Jones"]]
+
+let lesClay: [String : Any] = ["name": "Les Clay", "height": 42, "isExperienced": true, "guardians": ["Wynonna Brown"]]
+
+let herschelKrustofski: [String : Any] = ["name": "Herschel Krustofski", "height": 45, "isExperienced": true, "guardians": ["Hyman Krustofski", "Rachel Krustofski"]]
+
+let allPlayers = [
+  joeSmith, jillTanner, billBon, evaGordon, mattGill, kimmyStein,
+  sammyAdams, karlSaygan, suzaneGreenberg, salDali, joeKavalier, benFinkelstein,
+  diegoSoto, chloeAlaska, arnoldWillis, phillipHelm, lesClay, herschelKrustofski
+]
